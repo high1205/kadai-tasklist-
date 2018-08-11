e73b960ecf8a623531f9c190744b2bf16d22ccb7
toru.takahashi <asset.management.1983@gmail.com>
2018-08-11 01:02:22 +0000

    add new column status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true, length: { maximum: 10,
+    too_long: "は%{count}文字以内で入力して下さい。" }
 end
