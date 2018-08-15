a377fca1b447c2dfbb661b545a54b909a2209ff8
toru.takahashi <asset.management.1983@gmail.com>
2018-08-15 13:47:39 +0000

    add top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
   root to: 'tasks#index'
   
   resources :tasks
