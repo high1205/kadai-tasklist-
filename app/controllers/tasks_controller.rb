ee1d9c2c63c0fbc9eef45fc92b0150726a665b2f
toru.takahashi <asset.management.1983@gmail.com>
2018-08-16 14:19:45 +0000

    add correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show]
   
-  # def index
-  #   @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(20)
-  #   # @tasks = Task.order(created_at: :desc).page(params[:page]).per(20)
-  # end
+  def index
+    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(20)
+    # @tasks = Task.order(created_at: :desc).page(params[:page]).per(20)
+  end
 
   def show
     
@@ -57,6 +58,14 @@ class TasksController < ApplicationController
   # 共通化処理
   def set_task
     @task = Task.find(params[:id])
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      flash[:danger] = '自分のタスク以外は閲覧できません'
+      redirect_to root_url
+    end
   end
 
   # Strong Parameter
